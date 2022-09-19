8bbf3f04d2294b8cebd734f07aebdc2930227a2e
backcoder <backcoder2@gmail.com>
2022-09-19 12:20:38 +0900

    controller

---
+++ src/main/java/com/example/pullpush/testController.java
@@ -0,0 +1,16 @@
+package com.example.pullpush;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+	
+	@GetMapping("/test")
+	public String test () {
+		
+		
+		return "jsptest";
+	}
+
+}
